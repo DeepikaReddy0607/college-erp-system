fc4dca9712030dc2016cbde2e8b3363424e047dc
DeepikaReddy0607 <deepikareddyrudru@gmail.com>
2026-04-24 00:02:20 +0530

    Clean up email configuration in settings.py
    Removed commented-out email configuration settings.

--- erp/settings.py
+++ erp/settings.py
@@ -160,9 +160,5 @@ CELERY_TASK_SERIALIZER = 'json'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER = 'nitandhraerp@gmail.com'
-# EMAIL_HOST_PASSWORD = 'tqesrcvhmxiflqop'
+EMAIL_USE_TLS = True
